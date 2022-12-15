7af60226ac13f0ae14effc011ab06152e9c91740
Székely Balázs <szekelyb91@gmail.com>
2022-12-15 17:20:15 +0100

    Leírás hozzáadva

--- IPv4App.java
+++ IPv4App.java
@@ -1,12 +1,14 @@
 /**
  * @author Székely Balázs Csaba
  * 2022-12-15
+ * 
+ * Készítsünk egy programot, ami bekér egy IPv4 címet és elemzi azt, majd bizonyos megállapításokra jut
  */
 
 public class IPv4App {
 
     public static void main(String[] args) {
-    
+        System.out.print("Adjon meg egy IPv4 címet: ");
     }
     
 }
